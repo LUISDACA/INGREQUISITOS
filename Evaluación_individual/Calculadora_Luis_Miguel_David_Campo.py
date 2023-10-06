5176d4f668375838328046efc899e7ea4419f115
LUIS MIGUEL DAVID CAMPO <134025037+LUISDACA@users.noreply.github.com>
2023-10-06 10:13:43 -0500

    Create Calculadora_Luis_Miguel_David_Campo.py

---
+++ Evaluación_individual/Calculadora_Luis_Miguel_David_Campo.py
@@ -0,0 +1,15 @@
+#realizado por LUIS MIGUEL DAVID CAMPO
+#Calculadora
+print("Calculadora de suma, resta, multiplicación y división")
+print("Declare el primer número")
+num_1 = int(input())
+print("Declare el segundo número")
+num_2 = int(input())
+suma = num_1+num_2
+resta = num_1-num_2
+multiplicacion = num_1*num_2
+division = num_1/num_2
+print("El resultado de la suma: ",suma)
+print("El resultado de la resta: ",resta)
+print("El resultado de la multiplicación: ",multiplicacion)
+print("El resultado de la división: ",division)
